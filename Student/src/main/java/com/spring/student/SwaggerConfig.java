3abd4bfaf9b6381209b1448a08891d06caa8c8db
Your Nameprathishshetty <prathish350@gmail.com>
2024-06-12 12:38:53 +0530

    Swagger update

--- Student/src/main/java/com/spring/student/SwaggerConfig.java
+++ Student/src/main/java/com/spring/student/SwaggerConfig.java
@@ -1,23 +1,18 @@
 package com.spring.student;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
 
-@EnableSwagger2
+@Configuration
 @EnableWebMvc
-public class SwaggerConfig {
-	 public Docket SwaggerApi() { 
-	       return new Docket(DocumentationType.SWAGGER_2) 
-	               .select() 
-	               .apis(RequestHandlerSelectors.any()) 
-	               .paths(PathSelectors.any()) 
-	               .build(); 
-	   }
+@OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info
+.Info(title = "Student Service", version = "1.0", description = "API documentation"))
+public class SwaggerConfig implements WebMvcConfigurer{
 	
+	
+
 }
